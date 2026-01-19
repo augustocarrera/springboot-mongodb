1d7cdd28580634ba49a0a3a31803890093eb7957
Augusto Carrera <augusto.carrera@hotmail.com>
2026-01-19 12:42:34 -0300

    correções repositório post

--- src/main/java/com/augustocarrera/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/augustocarrera/workshopmongo/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.augustocarrera.workshopmongo.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.augustocarrera.workshopmongo.domain.User;
 
+
+@Repository
 public interface UserRepository extends MongoRepository<User, String>{
 
 }
